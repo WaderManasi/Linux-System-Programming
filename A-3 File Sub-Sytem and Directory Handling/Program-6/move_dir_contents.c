87e19d4909c36e84df0d01c538583f674b9277f9
Manasi Wader <manasiwader@gmail.com>
2020-09-23 12:49:02 +0530

    File Sub-System and Directory File Handling Programs

---
+++ A-3 File Sub-Sytem and Directory Handling/Program-6/move_dir_contents.c
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//                          LINUX SYSTEM PROGRAMMING WITH UNIX INTERNALS                     //
+//                                                                                           //
+//Name: Manasi Wader                                                                         //
+//Topic: Program which accept two directory names from user and move all files from          //
+//          source directory to destination directory.                                       //
+///////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+int main()
+{
+    return 0;
+}
+///////////////////////////////////////////////////////////////////////////////////////////////
+//compile: make build
+//run: make dir1="name" dir2="name" run
+///////////////////////////////////////////////////////////////////////////////////////////////
