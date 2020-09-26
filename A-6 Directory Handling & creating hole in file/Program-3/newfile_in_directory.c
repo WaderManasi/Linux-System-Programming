2eef56cd17e89eb1988787c3d23d936d3e7d4dd2
Manasi Wader <manasiwader@gmail.com>
2020-09-26 10:20:40 +0530

    Program 3

---
+++ A-6 Directory Handling & creating hole in file/Program-3/newfile_in_directory.c
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//                          LINUX SYSTEM PROGRAMMING WITH UNIX INTERNALS                     //
+//                                                                                           //
+//Name: Manasi Wader                                                                         //
+//Topic: Program which accept directory name and file name from user and create file in      //
+//          that directory.                                                                  //
+///////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<dirent.h>
+int main()
+{
+    return 0;
+}
+///////////////////////////////////////////////////////////////////////////////////////////////
+//compile: make build
+//run: make run
+///////////////////////////////////////////////////////////////////////////////////////////////
